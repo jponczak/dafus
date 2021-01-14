c21cd81129d259e02ffd25bec1831b5d19a7e153
Joe Ponczak <jponczak@gmail.com>
2021-01-14 12:17:47 -0500

    bug fixes

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  //  url: "mongodb://localhost:27017/loc"
-  url: "mongodb+srv://locAdmin:locTest@cluster0.0fpcw.mongodb.net/loc?retryWrites=true&w=majority"
+    url: "mongodb://localhost:27017/loc"
 };
 
 
 
-  //url: "mongodb+srv://locAdmin:locTest@cluster0.0fpcw.mongodb.net/loc?retryWrites=true&w=majority"
-    //mongodb+srv://locAdmin:<password>@cluster0.0fpcw.mongodb.net/<dbname>?retryWrites=true&w=majority
